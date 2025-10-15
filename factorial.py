d2eac8475e79b84b3a65698b6ef5780addde52df
Omoshalewa <Oaderibigbe0437@conestogac.on.ca>
2025-10-15 14:07:03 -0400

    Fix Pylint issues

--- factorial.py
+++ factorial.py
@@ -1,33 +1,23 @@
-# factorial.py
-
 import time
 
 final_list = []
+
 def factorial(n):
-
-   time.sleep(.1)
-   factorial = 1
-
-   for i in range (1,n+1):
-
-    factorial = factorial * i
-
-    return factorial
+    """Calculate the factorial of a given number n."""
+    time.sleep(0.1)
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+    return result
 
 def sum_factorial():
-
+    """Calculate the sum of factorials from 0 to 49."""
     for i in range(50):
-
         final_list.append(factorial(i))
 
-    result=sum(final_list)
-
-    print("Final SUM = {}".format(result))
-
+    result = sum(final_list)
+    print(f"Final SUM = {result}")
     return result
 
 if __name__ == "__main__":
-
     sum_factorial()
-
-    # Final SUM = 620960027832821612639424806694551108812720525606160920420940314
